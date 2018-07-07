a1d54f76db871afd04039d0382ec7a99b1b94c03
Moris Amon <amonmoris@gmail.com>
2018-07-07 19:24:09 +0300

    gamezone component added

---
+++ client/app/components/game-zone/gamezone.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-gamezone',
+  templateUrl: './gamezone.component.html',
+  styleUrls: ['./gamezone.component.css']
+})
+export class GamezoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
