3ffb456a875c1ba87a5cd03d58c3037946ad0d35
Moris Amon <amonmoris@gmail.com>
2018-07-12 20:29:16 +0300

    added generic play videos and change images

--- client/app/components/game-zone/gamezone.component.js
+++ client/app/components/game-zone/gamezone.component.js
@@ -11,20 +11,33 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
-var SRC = "/assets/vidoes/";
+var SRC = "/assets/videos/";
+var MAX_LEVEL = 3;
 var GameZoneAreaComponent = /** @class */ (function () {
-    function GameZoneAreaComponent(route, router) {
+    function GameZoneAreaComponent(route, router, elementRef) {
         var _this = this;
         this.route = route;
         this.router = router;
-        this.route.params.subscribe(function (params) { return _this.videoName = params; });
-        this.showImage = true;
-        this.showVidoe = false;
+        this.elementRef = elementRef;
+        this.subLevel = 1;
+        this.route.params.subscribe(function (params) {
+            _this.videoName = params;
+            _this.char = params.id.split('_')[0];
+            _this.level = params.id.split('_')[1].split('.')[0];
+            _this.videoSRC = _this.getSrcToShow('.mp4');
+            _this.imageSRC = _this.getSrcToShow('.png');
+        });
+        this.ShowImage();
     }
+    GameZoneAreaComponent.prototype.ngAfterViewInit = function () {
+        //this.videoplayer.nativeElement.play(); //works!
+    };
     GameZoneAreaComponent.prototype.ngOnInit = function () {
     };
-    GameZoneAreaComponent.prototype.getVideoToShow = function (src) {
-        console.log(SRC + this.videoName.id);
+    GameZoneAreaComponent.prototype.getSrcToShow = function (type) {
+        var finalSRC = SRC + this.charType(this.videoName.id) + "/" + this.videoName.id + type;
+        console.log(finalSRC);
+        return finalSRC;
     };
     GameZoneAreaComponent.prototype.changeFaceBtnStyle = function (organ, left, top, width, height) {
         if (organ == "face") {
@@ -34,6 +47,46 @@ var GameZoneAreaComponent = /** @class */ (function () {
             this.myEyeBtnStyles = { 'left': left, 'top': top, 'width': width, 'height': height };
         }
     };
+    GameZoneAreaComponent.prototype.charType = function (videoName) {
+        if (videoName.includes('player'))
+            return "Player";
+        if (videoName.includes('fireman'))
+            return "Fireman";
+        if (videoName.includes('superman'))
+            return "Superman";
+        if (videoName.includes('farmer'))
+            return "Farmer";
+        if (videoName.includes('superwoman'))
+            return "Superwoman";
+    };
+    GameZoneAreaComponent.prototype.StartVideo = function () {
+        this.ShowVideo();
+        this.videoplayer.nativeElement.play(); //works!
+    };
+    GameZoneAreaComponent.prototype.VideoEnded = function () {
+        console.log("The video is stoped");
+        this.subLevel++;
+        if (this.subLevel <= MAX_LEVEL) {
+            this.ChangeSources(this.subLevel);
+            this.ShowImage();
+        }
+    };
+    GameZoneAreaComponent.prototype.ShowImage = function () {
+        this.hiddenImage = false;
+        this.hiddenVideo = true;
+    };
+    GameZoneAreaComponent.prototype.ShowVideo = function () {
+        this.hiddenImage = true;
+        this.hiddenVideo = false;
+    };
+    GameZoneAreaComponent.prototype.ChangeSources = function (count) {
+        this.imageSRC = SRC + this.charType(this.videoName.id) + "/" + this.char + "_" + this.level + "." + this.subLevel + '.png';
+        this.videoSRC = SRC + this.charType(this.videoName.id) + "/" + this.char + "_" + this.level + "." + this.subLevel + '.mp4';
+    };
+    __decorate([
+        core_1.ViewChild('videoPlayer'),
+        __metadata("design:type", Object)
+    ], GameZoneAreaComponent.prototype, "videoplayer", void 0);
     GameZoneAreaComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
@@ -41,7 +94,7 @@ var GameZoneAreaComponent = /** @class */ (function () {
             templateUrl: './gamezone.component.html',
             styleUrls: ['./gamezone.component.css']
         }),
-        __metadata("design:paramtypes", [router_1.ActivatedRoute, router_1.Router])
+        __metadata("design:paramtypes", [router_1.ActivatedRoute, router_1.Router, core_1.ElementRef])
     ], GameZoneAreaComponent);
     return GameZoneAreaComponent;
 }());
--- client/app/components/game-zone/gamezone.component.ts
+++ client/app/components/game-zone/gamezone.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, NgModule, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Json } from '@angular/core/src/facade/lang';
 
-const SRC:String="/assets/vidoes/";
 
+
+const SRC:String="/assets/videos/";
+const MAX_LEVEL = 3;
 
 @Component({
   moduleId: module.id,
@@ -13,25 +15,43 @@ const SRC:String="/assets/vidoes/";
 })
 
 
-export class GameZoneAreaComponent implements OnInit {
+export class GameZoneAreaComponent implements OnInit, AfterViewInit{
 
+  ngAfterViewInit(): void {
+    //this.videoplayer.nativeElement.play(); //works!
+  }
+  @ViewChild('videoPlayer') videoplayer: any;
+  
   public videoName: any;
-  public showImage: Boolean;
-  public showVidoe: Boolean;
+  public hiddenImage: Boolean;
+  public hiddenVideo: Boolean;
   public myFaceBtnStyles: Json;
   public myEyeBtnStyles: Json;
+  public subLevel = 1;
+  public videoSRC: String;
+  public imageSRC: String;
+  public level: String;
+  public char: String
 
-  constructor(private route: ActivatedRoute, private router:Router) {
-    this.route.params.subscribe(params => this.videoName = params);
-    this.showImage = true;
-    this.showVidoe = false;
+  
+  constructor(private route: ActivatedRoute, private router:Router,private elementRef: ElementRef) {
+    this.route.params.subscribe((params) =>{
+      this.videoName = params
+      this.char = params.id.split('_')[0];
+      this.level = params.id.split('_')[1].split('.')[0];
+      this.videoSRC = this.getSrcToShow('.mp4');
+      this.imageSRC = this.getSrcToShow('.png');
+    });
+    this.ShowImage();
   }
 
   ngOnInit() {
   }
 
-  getVideoToShow(src: String){
-      console.log(SRC+this.videoName.id);
+  getSrcToShow(type: String){
+      var finalSRC = SRC + this.charType(this.videoName.id) + "/" + this.videoName.id + type;
+      console.log(finalSRC);
+      return finalSRC;
   }
 
   changeFaceBtnStyle(organ, left, top, width, height){
@@ -41,6 +61,51 @@ export class GameZoneAreaComponent implements OnInit {
     else if(organ == "eye"){
       this.myEyeBtnStyles={'left':left,'top':top,'width':width,'height':height};
     }
+  }
+
+  private charType(videoName){
+    if(videoName.includes('player'))
+      return "Player";
+    if(videoName.includes('fireman'))
+      return "Fireman";
+    if(videoName.includes('superman'))
+      return "Superman";
+    if(videoName.includes('farmer'))
+      return "Farmer";
+    if(videoName.includes('superwoman'))
+      return "Superwoman";
+  }
+
+  StartVideo(){
+    this.ShowVideo();
+    if(this.subLevel<=MAX_LEVEL){
+      this.videoplayer.nativeElement.play();
+    }
+  }
+
+  VideoEnded(){
+    console.log("The video is stoped");
+    this.subLevel++;
+    if(this.subLevel<=MAX_LEVEL){
+      this.ChangeSources(this.subLevel);
+      this.ShowImage();
+    }
+
+  }
+
+  ShowImage(){
+    this.hiddenImage = false;
+    this.hiddenVideo = true;
+  }
+
+  ShowVideo(){
+    this.hiddenImage = true;
+    this.hiddenVideo = false;
+  }
+
+  ChangeSources(count){
+    this.imageSRC = SRC + this.charType(this.videoName.id) + "/" + this.char + "_" + this.level + "." + this.subLevel + '.png';
+    this.videoSRC = SRC + this.charType(this.videoName.id) + "/" + this.char + "_" + this.level + "." + this.subLevel + '.mp4';
   }
 
 }
