b7888661e027ce1125f65c2d8689ab0e714e1957
Moris Amon <amonmoris@gmail.com>
2019-03-10 14:37:31 +0200

    Change logic on the audio records

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { SvmVectorService } from './admin/services/svmvectors.service';
 import { svmVector } from './components/game-zone/variables';
 import * as io from 'socket.io-client';
 
+
 @Component({
   selector: 'my-app',
   templateUrl: 'app.component.html',
   styleUrls:['./app.component.css'],
   providers:[ DataService ]
 })
+
 
 export class AppComponent {
   
---
+++ src/app/helper/audioCounter.ts
@@ -0,0 +1,45 @@
+export const configAudioPerCharacter = {"fireman_boy_1_1" : 5,
+                                        "fireman_boy_1_2" : 6,
+                                        "fireman_boy_1_3" : 7,
+                                        "fireman_boy_1_4" : 10,
+
+                                        "fireman_boy_2_1" : 5,
+                                        "fireman_boy_2_2" : 6,
+                                        "fireman_boy_2_3" : 7,
+                                        "fireman_boy_2_4" : 10,
+
+                                        "fireman_boy_3_1" : 5,
+                                        "fireman_boy_3_2" : 6,
+                                        "fireman_boy_3_3" : 7,
+                                        "fireman_boy_3_4" : 11,
+
+                                        "farmer_boy_1_1" : 5,
+                                        "farmer_boy_1_2" : 6,
+                                        "farmer_boy_1_3" : 7,
+                                        "farmer_boy_1_4" : 10,
+
+                                        "farmer_boy_2_1" : 6,
+                                        "farmer_boy_2_2" : 7,
+                                        "farmer_boy_2_3" : 8,
+                                        "farmer_boy_2_4" : 11,
+
+                                        "farmer_boy_3_1" : 5,
+                                        "farmer_boy_3_2" : 6,
+                                        "farmer_boy_3_3" : 7,
+                                        "farmer_boy_3_4" : 11,
+
+                                        "superman_boy_1_1" : 7,
+                                        "superman_boy_1_2" : 8,
+                                        "superman_boy_1_3" : 9,
+                                        "superman_boy_1_4" : 11,
+
+                                        "superman_boy_2_1" : 4,
+                                        "superman_boy_2_2" : 5,
+                                        "superman_boy_2_3" : 6,
+                                        "superman_boy_2_4" : 9,
+
+                                        "superman_boy_3_1" : 7,
+                                        "superman_boy_3_2" : 8,
+                                        "superman_boy_3_3" : 9,
+                                        "superman_boy_3_4" : 13
+                                      };
