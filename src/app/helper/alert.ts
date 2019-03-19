7b3a448a4f914d8421f43aa5650439f66fa385b9
Moris Amon <amonmoris@gmail.com>
2019-03-20 00:39:17 +0200

    Added logic for popup

--- src/app/helper/alert.ts
+++ src/app/helper/alert.ts
@@ -1,11 +1,7 @@
 export const configAlert = {
-    "fireman_boy_1_1" : "stop",
-    "fireman_boy_1_2" :"stop",
-    "fireman_boy_1_3" : "stop",
-    "fireman_boy_1_4" : "stop",
-    "fireman_boy_1_5" : "stop",
-    "fireman_boy_1_6" : "stop",
-    "fireman_boy_1_7" : "stop",
-    "fireman_boy_1_8" : "stop",
-    "fireman_boy_1_9" : "stop",
+    "fireman_boy_1_3.mp3" : "eyes",
+    "fireman_boy_1_4.mp3" : "eyes",
+    "fireman_boy_1_5.mp3" : "eyes",
+    "fireman_boy_1_9.mp3" : "stop",
+    "fireman_boy_1_10.mp3" : "stop",
 }
